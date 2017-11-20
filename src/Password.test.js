6d523804e50fc0a16a1b05262b15433ca8a0cc98
dennymiloen <dennymiloe@gmail.comn>
2017-11-20 16:56:19 +0700

    Password test changed

--- src/Password.test.js
+++ src/Password.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 import { PasswordModal } from './components/PasswordModal';
+import PasswordFields from './components/PasswordFields';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -17,9 +18,27 @@ const initialState = {
   passwordLength: false
 };
 
+const passwordModal = new PasswordModal();
+
+const passwordModalComponent = shallow(
+  <PasswordModal show={ true }/>
+);
+
 describe('PasswordModal component', () => {
   test("Initial state check", () => {
-    const wrapper = shallow(<PasswordModal/>);
-    expect(wrapper.state()).toEqual(initialState);
+    expect(passwordModalComponent.state()).toEqual(initialState);
+  });
+
+  test("oneLowercase state should equal true", () => {
+    const passwordFieldsComponent = shallow(
+      <PasswordFields
+        newPassword={ initialState.newPassword }
+        handleInputChange={ passwordModal.handleInputChange }
+      />
+    );
+
+    const urlTextBox = passwordFieldsComponent.find('input').at(1);
+
+    urlTextBox.simulate('change', { target: { value: 'dimitri' } });
   });
 });
